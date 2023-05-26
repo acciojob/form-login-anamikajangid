1b09de7dafbf01a54ff45cc179d4ec8f229ec473
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-05-26 23:46:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+let firstname=document.getElementById("fname").value;
+let lastname=document.getElementById("lname").value;
 function getFormvalue() {
-    let firstname=document.getElementsById("fname").value;
-	let lastname=document.getElementById("lname").value;
+    
 	alert(firstname + " " +lastname);
 
 }
